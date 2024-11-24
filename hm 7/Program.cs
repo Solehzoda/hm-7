443cec27983afe3f6cc32df36badfbc688c88b08
Solehzoda <salikhovajillo@gmail.com>
2024-11-24 14:58:28 +0500

    первый коммит

---
+++ hm 7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hm_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var result = a >= 10 && a <= 99 && a % 2 != 0;
+            Console.WriteLine(result);
+        }
+    }
+}
